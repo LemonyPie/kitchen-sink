c47a69a1a53dabea904411884db0e8d7336029b5
Artemee <artemee.senin@gmail.com>
2020-04-04 19:20:41 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,11 +1,11 @@
 sudo apt-get update
-sudo apt-get insall -y neovim zsh
+sudo apt-get install -y neovim zsh
 
 mkdir ~/.config/nvim
 printf "set runtimepath^=~/.vim runtimepath+=~/.vim/after \nlet &packpath = &runtimepath \nsource ~/.vimrc" > ~/.config/nvim/init.vim
 printf "set tabstop=4\nset shiftwidth=4\nset smarttab\nset expandtab\nset smartindent" > ~/.vimrc
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+yes "yes" | sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
 ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
 sed -e "s/robbyrussel/spaceship" ~/.zshrc
