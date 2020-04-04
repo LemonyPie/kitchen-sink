ee3ce6287452c0cf4568d2a93ef7990941f2195b
Artemee <artemee.senin@gmail.com>
2020-04-04 20:47:50 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -24,3 +24,9 @@ sudo add-apt-repository \
    stable"
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+sudo curl -L https://raw.githubusercontent.com/docker/compose/1.25.4/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
+sudo docker version
+sudo docker-compose version
